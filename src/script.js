1d7aa3d797d943e8a09f856486dc232cd2e22cb0
Mohsen Afshari <mo3nafshari75@gmail.com>
2025-09-01 15:46:29 +0330

    Fixes and stability

--- src/script.js
+++ src/script.js
@@ -1,7 +1,25 @@
 const isLoaded = { current: false };
+let windowResizeTimeoutID;
 
 document.addEventListener("DOMContentLoaded", () => {
   isLoaded.current = true;
+
+  initializeOnFontLoaded();
+
+  // Determine what color scheme user wants
+  // Listener on system preference
+  // Adds transition class
+  initializeColorScheme();
+
+  // initialize the language settings
+  initiateLangauage(document.documentElement.lang);
+
+  // update switches for english/persian and light/dark mode
+  // And define change listeners on them
+  initializeSwitches();
+
+  // Substitute start/end date with text
+  updateDateTexts();
 
   // Populate svgs that will clutter up the HTML if placed directly
   populateSVGs();
@@ -13,67 +31,70 @@ document.addEventListener("DOMContentLoaded", () => {
   animateTimeLines();
 
   // Resize listener for correction for lines connecting two of the circles in the timeline
-  addResizeListener();
+  initilizeWindowResizeListener();
 });
 
-// remove animation for timeline if user zooms in on the page we don't want to have the transform animating
-window.onload = () => {
-  const timelineElements = document.querySelectorAll(
-    ".timeline__begin__animation"
+function initializeColorScheme() {
+  const isDarkMode =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  changeColorScheme(isDarkMode);
+
+  document.documentElement.addEventListener(
+    "transitionend",
+    () => {
+      document.documentElement.classList.add("color__transition");
+    },
+    { once: true }
   );
-  for (const element of timelineElements) {
-    element.classList.remove("timeline__begin__animation");
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+      changeColorScheme(event.matches);
+    });
+}
+
+function changeColorScheme(isDarkMode) {
+  if (isDarkMode) {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+  } else if (!isDarkMode) {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
   }
-};
+}
+
+function initializeOnFontLoaded() {
+  document.fonts.onloadingdone = () => {
+    for (const element of document.querySelectorAll(".career__timeline")) {
+      animateCareerTimeline(element);
+
+      const beginElement = document.querySelector(".timeline__begin");
+
+      // after the initial animation I need to remove the transition class so that it stops the transition when window resizes
+      beginElement.addEventListener(
+        "transitionend",
+        () => {
+          beginElement.classList.remove("timeline__begin__animation");
+        },
+        { once: true }
+      );
+    }
+
+    // The sizes of english elements might be wrong and need recalculations
+    handleEnglishAndPersianElementSizes(document.documentElement.lang);
+
+    // Transition classes should be added after initialization is complete as to avoid undesired animations
+    addTransitionClasses();
+  };
+}
 
 function animateTimeLines() {
-  const timelineElements = document.querySelectorAll(".career__timeline");
-
-  for (const element of timelineElements) {
+  for (const element of document.querySelectorAll(".career__timeline")) {
     animateCareerTimeline(element);
   }
-}
-
-function imgLoaderListener() {
-  document.querySelectorAll("img").forEach((img) => {
-    if (img.complete) {
-      img.closest("div")?.classList.remove("loading");
-    } else {
-      img.onload = () => {
-        img.closest("div")?.classList.remove("loading");
-      };
-    }
-  });
-}
-
-function populateSVGs() {
-  fetchSVG("/assets/Jahad.svg").then((svg) => {
-    insertElement(svg, "jahad");
-  });
-
-  fetchSVG("/assets/Simaran.svg").then((svg) => {
-    insertElement(svg, "simaran");
-  });
-
-  fetchSVG("/assets/Jquery.svg").then((svg) => {
-    insertElement(svg, "jquery");
-  });
-
-  fetchSVG("/assets/Processmaker.svg").then((svg) => {
-    insertElement(svg, "processmaker");
-  });
-
-  fetchSVG("/assets/Language.svg").then((svg) => {
-    insertElement(svg, "language");
-  });
-
-  fetchSVG("/assets/Darkmode.svg").then((svg) => {
-    insertElement(svg, "dark");
-  });
-
-  fetchSVG("/assets/Lightmode.svg").then((svg) => {
-    insertElement(svg, "light");
-  });
 }
 
 function animateCareerTimeline(element) {
@@ -92,14 +113,46 @@ function animateCareerTimeline(element) {
   beginElement.style.transform = `translateY(${tranlateValue}px)`;
 }
 
-function addResizeListener() {
-  window.addEventListener("resize", () => {
-    const elements = document.querySelectorAll(".career__timeline");
-
-    for (const element of elements) {
-      animateCareerTimeline(element);
+function imgLoaderListener() {
+  document.querySelectorAll("img").forEach((img) => {
+    if (img.complete) {
+      img.closest("div")?.classList.remove("loading");
+    } else {
+      img.onload = () => {
+        img.closest("div")?.classList.remove("loading");
+      };
     }
   });
+}
+
+function populateSVGs() {
+  const svgAndElements = Array.from(
+    document.querySelectorAll("[data-svg-url]")
+  ).reduce((result, elem) => {
+    const svgUrl = elem.dataset.svgUrl;
+
+    if (svgUrl === undefined) {
+      console.error(`element: ${elem}`);
+      throw new Error("Url of the element not found");
+    }
+
+    if (result[svgUrl] === undefined) {
+      result[svgUrl] = [elem];
+    } else {
+      result[svgUrl].push(elem);
+    }
+
+    return result;
+  }, {});
+
+  for (const url in svgAndElements) {
+    fetchSVG(url).then((svg) => {
+      svgAndElements[url].forEach((element) => {
+        element.append(svg.cloneNode(true));
+        element.classList.remove("loading");
+      });
+    });
+  }
 }
 
 async function fetchSVG(url) {
@@ -125,88 +178,65 @@ async function fetchSVG(url) {
   });
 }
 
-function insertElement(svg, id) {
-  const intervalID = setInterval(() => {
-    if (isLoaded.current) {
-      clearInterval(intervalID);
-      const elements = document.querySelectorAll(`#${id}`);
-
-      elements.forEach((element, i) => {
-        element.appendChild(i === 0 ? svg : svg.cloneNode(true));
-
-        if (element.classList.contains("loading")) {
-          element.classList.remove("loading");
-        }
-      });
+function initilizeWindowResizeListener() {
+  window.addEventListener("resize", () => {
+    if (!windowResizeTimeoutID) {
+      removeTransitionClasses();
+    } else {
+      clearTimeout(windowResizeTimeoutID);
     }
-  }, 10);
+
+    for (const element of document.querySelectorAll(".career__timeline")) {
+      animateCareerTimeline(element);
+    }
+
+    handleEnglishAndPersianElementSizes(document.documentElement.lang);
+
+    windowResizeTimeoutID = setTimeout(() => {
+      addTransitionClasses();
+      windowResizeTimeoutID = undefined;
+    }, 300);
+  });
 }
 
-document.querySelector("#languageCheckbox").addEventListener("change", (e) => {
-  const isEnglish = e.target.checked;
+function initializeSwitches() {
+  const languageCheckbox = document.querySelector("#languageCheckbox");
+  const lightModeCheckbox = document.querySelector("#lightModeCheckbox");
 
-  changeLanguage(isEnglish ? "en" : "fa");
-});
-document.querySelector("#languageCheckbox").checked === "en"
-  ? initialENPrep()
-  : initialFAPrep();
+  languageCheckbox.addEventListener("change", (e) => {
+    const isEnglish = e.target.checked;
 
-// Why not make both the Fa and En elements absolute
-// Because I lose the dynamic sizing of the main element
-// I want Fa to be not absolute so it can take some space from main element
+    changeLanguage(isEnglish ? "en" : "fa");
+  });
 
-// initial Fa
-function initialFAPrep() {
+  lightModeCheckbox.addEventListener("change", (e) => {
+    const isDarkMode = !e.target.checked;
+
+    changeColorScheme(isDarkMode);
+  });
+
+  languageCheckbox.checked = document.documentElement.lang === "en";
+
+  lightModeCheckbox.checked =
+    document.documentElement.classList.contains("light");
+}
+
+function initiateLangauage(lang) {
   const farsiElement = document.querySelector("#info-farsi");
   const englishElement = document.querySelector("#info-english");
 
+  // Why not make both the Fa and En elements absolute
+  // Because I lose the dynamic sizing of the main element
+  // I want Fa to be not absolute so it can take some space from main element
   englishElement.style.position = "absolute";
   englishElement.style.height = "100%";
   englishElement.style.opacity = 0;
   englishElement.style.width = `${farsiElement.offsetWidth}px`;
   englishElement.style.left = `-${farsiElement.offsetWidth - 16}px`; // take out one margin
 
-  document.documentElement.lang = "fa";
-  document.documentElement.classList.remove("page__lang__en");
-  document.documentElement.classList.add("page__lang__fa");
-
-  changeLanguageMain("fa");
-  changeLanguageHeader("fa");
-  changeLanguageDateTimeline("fa");
-
-  addTransitionToPersonalWrappers();
-}
-
-function initialENPrep() {
-  const farsiElement = document.querySelector("#info-farsi");
-  const englishElement = document.querySelector("#info-english");
-
-  englishElement.style.width = `${farsiElement.offsetWidth}px`;
-
-  farsiElement.style.position = "absolute";
-  farsiElement.style.height = "100%";
-  farsiElement.style.opacity = 0;
-  farsiElement.style.right = `16px`;
-
-  document.documentElement.lang = "en";
-  document.documentElement.classList.remove("page__lang__fa");
-  document.documentElement.classList.add("page__lang__en");
-
-  changeLanguageMain("en");
-  changeLanguageHeader("en");
-  changeLanguageDateTimeline("en");
-
-  addTransitionToPersonalWrappers();
-}
-
-function addTransitionToPersonalWrappers() {
-  setTimeout(() => {
-    Array.from(document.querySelectorAll(".personal__wrapper")).forEach(
-      (elem) => {
-        elem.classList.add("personal__wrapper__transition");
-      }
-    );
-  });
+  changeLanguageMain(lang);
+  changeLanguageHeader(lang);
+  changeLanguageDateTimeline(lang);
 }
 
 function changeLanguage(lang) {
@@ -214,6 +244,7 @@ function changeLanguage(lang) {
   changeLanguageMain(lang);
   changeLanguageHeader(lang);
   changeLanguageDateTimeline(lang);
+  changeLanguageTitle(lang);
 }
 
 function changeLanguageOuterLayer(lang) {
@@ -248,6 +279,8 @@ function changeLanguageMain(lang) {
     const englishInfoSection = job.querySelector(".career__info__english");
     const careerTimeline = job.querySelector(".career__timeline");
 
+    englishInfoSection.style.width = persianInfoSection.offsetWidth + "px";
+
     if (lang === "en") {
       persianInfoSection.style.transform = `translateX(${persianInfoSection.offsetWidth}px)`;
       persianInfoSection.style.opacity = 0;
@@ -270,19 +303,90 @@ function changeLanguageMain(lang) {
   }
 }
 
-// Same width for english element as persian one
-Array.from(document.querySelectorAll(".career__section")).forEach((job) => {
-  const persianInfoSection = job.querySelector(".career__info__farsi");
-  const englishInfoSection = job.querySelector(".career__info__english");
+function getMainHeight() {
+  const main = document.querySelector("main");
 
-  englishInfoSection.style.width = persianInfoSection.offsetWidth + "px";
+  const persianHeaderElement = main.querySelector(".main__header__persian");
+  const englishHeaderElement = main.querySelector(".main__header__english");
 
-  persianInfoSection.classList.add("career__info__transition");
-  englishInfoSection.classList.add("career__info__transition");
-});
+  const persianHeaderComputedStyles =
+    window.getComputedStyle(persianHeaderElement);
+  const englishHeaderComputedStyles =
+    window.getComputedStyle(englishHeaderElement);
 
-// Substitute start/end date with text
-updateDateTexts();
+  const headerMarginBottom = Math.max(
+    parseFloat(persianHeaderComputedStyles.marginBottom),
+    parseFloat(englishHeaderComputedStyles.marginBottom)
+  );
+
+  // header height
+  const headerHeight = Math.max(
+    persianHeaderElement.offsetHeight +
+      parseFloat(persianHeaderComputedStyles.marginTop),
+    englishHeaderElement.offsetHeight +
+      parseFloat(englishHeaderComputedStyles.marginTop)
+  );
+
+  // common margin between header and first job
+  const firstCareerSection = main.querySelector(".career__section");
+  const firstCareerSectionStyles = window.getComputedStyle(firstCareerSection);
+  const headerAndFirstJobCommonMargin = Math.max(
+    headerMarginBottom,
+    parseFloat(firstCareerSectionStyles.marginBottom)
+  );
+
+  // Sum of all jobs heights
+  const jobs = Array.from(document.querySelectorAll(".career__section"));
+  const sumOfJobsHeight = jobs.reduce((result, job, i) => {
+    const farsiElement = job.querySelector(".career__info__farsi");
+    const farsiElementStyles = getComputedStyle(
+      farsiElement.closest(".career__section")
+    );
+
+    const englishElement = job.querySelector(".career__info__english");
+    const englishElementStyles = getComputedStyle(
+      englishElement.closest(".career__section")
+    );
+
+    const farsiEnglishElementHeight = Math.max(
+      farsiElement.offsetHeight,
+      englishElement.offsetHeight
+    );
+
+    const nextJob = jobs.at(i + 1);
+    let commonMargin = 0;
+    if (nextJob !== undefined) {
+      const nextJobFarsiElement = nextJob
+        .querySelector(".career__info__farsi")
+        .closest(".career__section");
+
+      const nextJobEnglishElement = nextJob
+        .querySelector(".career__info__english")
+        .closest(".career__section");
+
+      const nextJobFarsiElementStyles =
+        window.getComputedStyle(nextJobFarsiElement);
+      const nextJobEnglishElementStyles = window.getComputedStyle(
+        nextJobEnglishElement
+      );
+
+      commonMargin = Math.max(
+        Math.max(
+          parseFloat(nextJobFarsiElementStyles.marginTop),
+          parseFloat(farsiElementStyles.marginBottom)
+        ),
+        Math.max(
+          parseFloat(nextJobEnglishElementStyles.marginTop),
+          parseFloat(englishElementStyles.marginBottom)
+        )
+      );
+    }
+
+    return result + farsiEnglishElementHeight + commonMargin;
+  }, 0);
+
+  return headerHeight + headerAndFirstJobCommonMargin + sumOfJobsHeight + 128;
+}
 
 function updateDateTexts() {
   Array.from(document.querySelectorAll("[data-start-date]")).forEach(
@@ -371,17 +475,6 @@ function getDateTexts(props) {
   return { englishStartDate, englishEndDate, persianStartDate, persianEndDate };
 }
 
-// Add transition class to header
-setTimeout(() => {
-  Array.from(
-    document.querySelectorAll(".main__header__persian, .main__header__english")
-  ).forEach((elem) => {
-    if (!elem.classList.contains("main__header__transition")) {
-      elem.classList.add("main__header__transition");
-    }
-  });
-}, 0);
-
 function changeLanguageHeader(lang) {
   const englishHeader = document.querySelector(".main__header__english");
   const persianHeader = document.querySelector(".main__header__persian");
@@ -401,22 +494,67 @@ function changeLanguageHeader(lang) {
   }
 }
 
-document.querySelector("#lightModeCheckbox").addEventListener("change", (e) => {
-  const isLightMode = e.target.checked;
+function addTransitionClasses() {
+  document
+    .querySelector(".main__wrapper")
+    .classList.add("main__wrapper__transition");
 
-  changeDarkMode(isLightMode);
-});
+  for (const elem of document.querySelectorAll(".personal__wrapper")) {
+    elem.classList.add("personal__wrapper__transition");
+  }
 
-changeDarkMode(
-  document.documentElement.classList.contains("dark") ? false : true
-);
+  for (const elem of document.querySelectorAll(".career__section")) {
+    const persianInfoSection = elem.querySelector(".career__info__farsi");
+    const englishInfoSection = elem.querySelector(".career__info__english");
 
-function changeDarkMode(isLightMode) {
-  if (isLightMode) {
-    document.documentElement.classList.remove("dark");
-    document.documentElement.classList.add("light");
+    persianInfoSection.classList.add("career__info__transition");
+    englishInfoSection.classList.add("career__info__transition");
+  }
+
+  for (const elem of document.querySelectorAll(
+    ".main__header__persian, .main__header__english"
+  )) {
+    elem.classList.add("main__header__transition");
+  }
+
+  for (const elem of document.querySelectorAll(".career__timeline")) {
+    elem.classList.add("timeline__language__transition");
+  }
+}
+
+function removeTransitionClasses() {
+  for (const elem of document.querySelectorAll(
+    ".personal__wrapper__transition, .career__info__transition, .main__header__transition, .main__wrapper__transition, .timeline__language__transition"
+  )) {
+    elem.classList.remove(
+      "personal__wrapper__transition",
+      "career__info__transition",
+      "main__header__transition",
+      "main__wrapper__transition",
+      "timeline__language__transition"
+    );
+  }
+}
+
+function handleEnglishAndPersianElementSizes(lang) {
+  const infoFarsi = document.querySelector("#info-farsi");
+  const infoEnglish = document.querySelector("#info-english");
+  infoEnglish.style.width = `${infoFarsi.offsetWidth}px`;
+  infoEnglish.style.left = `-${infoFarsi.offsetWidth - 16}px`; // take out one margin
+
+  changeLanguage(lang);
+
+  const mainHeight = getMainHeight();
+  console.log(mainHeight);
+
+  const main = document.querySelector("main");
+  main.style.minHeight = mainHeight + "px";
+}
+
+function changeLanguageTitle(lang) {
+  if (lang === "fa") {
+    document.title = "محسن افشاری";
   } else {
-    document.documentElement.classList.remove("light");
-    document.documentElement.classList.add("dark");
+    document.title = "Mohsen Afshari";
   }
 }
